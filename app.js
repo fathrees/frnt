63d5b499f7af977ed5248614a1defb853539fa08
Ruslan Vasylenko <ruslan.vasylenko87@gmail.com>
2017-02-26 21:58:58 +0200

    storing new ads to mongodb via mongoose

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const { olx, categories } = require( './olx/olx');
 const express = require('express');
 const _ = require('lodash');
+
+const { olx, categories } = require( './olx/olx');
+
+const { mongoose } = require('./db/mongoose');
+const { Ad } = require('./db/models');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,10 +22,22 @@ app.get('/olx/realtyRentFlatLong/:city', (req, res) => {
   	}
   	Promise.all(promises).then((resolvesArr) => {
   		const allAdRefs = resolveObj.adRefs.concat(_.flatten(resolvesArr));
+  		Ad.find().then((ads) => {
+  			const newAdRefs = _.difference(allAdRefs, ads.map(ad => ad.ref));
+  			if (newAdRefs.length) {
+  				const newAdObjs = newAdRefs.map(ref => ({ ref }));
+  				Ad.collection.insert(newAdObjs, (err, res1) => {
+  					console.log(!err && `${res1.insertedCount} new ads were successfully stored`);
+  				});
+  			} else console.log('No new ads');
+  		}).catch(e => console.log('Unable connect to db', e));
   		res.send(allAdRefs);
-  	});
+  	}).catch((e) => res.send(e));
   }).catch((e) => res.send(e));
 }); 
 
 app.listen(`${port}`);
 console.log(`Server up on port ${port}`);
+
+//https://www.olx.ua/ajax/misc/contact/phone/qGRvE/
+//https://www.olx.ua/ajax/misc/contact/desc/qGRvE/
